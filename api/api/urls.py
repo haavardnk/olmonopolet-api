35adbc9704de312942135c2a93de429adcf21a46
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2025-10-20 12:37:19 +0200

    chore: remove unused notifications

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 
-
 urlpatterns = [
     re_path(r"^admin/shell/", include("django_admin_shell.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("beers.api.urls")),
-    path("notifications", include("notifications.api.urls")),
 ]
