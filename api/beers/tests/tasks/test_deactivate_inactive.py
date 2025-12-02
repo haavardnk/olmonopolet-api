207f48f45e69bbd4758c53ac0ff7e122a105d6ce
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2025-12-02 13:21:15 +0100

    chore: stop recently added from becoming inactive

--- api/beers/tests/tasks/test_deactivate_inactive.py
+++ api/beers/tests/tasks/test_deactivate_inactive.py
@@ -11,6 +11,27 @@ def test_deactivate_inactive_beer() -> None:
     """
     Test that a beer which is no longer on vinmonopolet gets deactivated.
     """
+    beer = Beer.objects.create(
+        vmp_id=12611502,
+        vmp_name="Ayinger Winterbock",
+        active=True,
+        vmp_updated=timezone.now() - timedelta(days=31),
+    )
+    Beer.objects.filter(pk=beer.pk).update(
+        created_at=timezone.now() - timedelta(days=15)
+    )
+
+    deactivate_inactive(30)
+
+    beer.refresh_from_db()
+    assert not beer.active
+
+
+@pytest.mark.django_db
+def test_new_beer_does_not_get_deactivated() -> None:
+    """
+    Test that a beer created less than 10 days ago does not get deactivated.
+    """
     Beer.objects.create(
         vmp_id=12611502,
         vmp_name="Ayinger Winterbock",
@@ -21,7 +42,7 @@ def test_deactivate_inactive_beer() -> None:
     deactivate_inactive(30)
 
     beer = Beer.objects.get(vmp_id=12611502)
-    assert not beer.active
+    assert beer.active
 
 
 @pytest.mark.django_db
