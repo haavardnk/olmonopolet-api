d18eb40c009701b34c7bd4af5c02397681eed1ad
Håvard Kråkenes <29313194+haavardnk@users.noreply.github.com>
2026-02-03 21:13:13 +0100

    chore: update docker entrypoint

--- api/run.sh
+++ api/run.sh
@@ -3,7 +3,7 @@
 uv run manage.py migrate
 
 # Start qcluster in the background
-nohup uv run manage.py qcluster &
+uv run manage.py qcluster &
 
-# Start Gunicorn server
-uv run gunicorn --limit-request-line 8000 --timeout 60 --bind :7000 --workers 5 api.wsgi:application
+# Start Gunicorn server (foreground for docker logs)
+uv run gunicorn --limit-request-line 8000 --timeout 60 --bind :7000 --workers 5 --capture-output --access-logfile - --error-logfile - api.wsgi:application
